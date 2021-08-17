8904123b05a277c6effa906a10efab8b84f75cb2
faiz <faizdhanani02@gmail.com>
2021-08-17 17:06:03 +0530

    yet another commit

--- finalproject/urls.py
+++ finalproject/urls.py
@@ -25,6 +25,4 @@ from django.conf.urls import url
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("capstone.urls")),
-    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', { 'document_root': settings.STATIC_ROOT, }),
-    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', { 'document_root': settings.MEDIA_ROOT, }),
 ]
